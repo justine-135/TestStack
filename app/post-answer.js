0e986f3ab6d7da84992ebfffd74d55c6c402467d
Justine <ujusine@yahoo.com>
2021-11-12 18:45:05 +0800

    toggle active button

--- app/post-answer.js
+++ app/post-answer.js
@@ -1,22 +1,29 @@
 window.addEventListener("load", ()=>{
     const postBtn = document.querySelector(".post-btn");
-    const editable = document.querySelector(".note-editable").childNodes;
-    const answersForum = document.querySelector(".answer-result");
+    const editable = document.querySelector(".note-editable");
+    const answersResult = document.querySelector(".answer-result");
+    const answerForum = document.querySelector(".answers-forum");
+    const count = document.querySelector(".all-answer-count");
+    const label = document.querySelector('.answer-label');
 
     postBtn.addEventListener('click', ()=>{
-        // for (let node of editable){
-        //     answersForum.appendChild(node);
-        //     console.log(node);
-        // }
 
-        editable.forEach((child)=>{
+        let cln = editable.cloneNode(true);
 
-            answersForum.appendChild(child);
-         
-            
-        })
+        cln.setAttribute("class", "result-clone");
+        cln.setAttribute("contenteditable", "false");
 
 
-
+        answersResult.appendChild(cln);
     })
+    
+    const updateCount = () => {
+        if (answerForum.childElementCount > 1){
+            label.innerHTML = "Answers";
+        }
+        count.innerHTML = answerForum.childElementCount;
+
+    }
+
+    updateCount();
 })
