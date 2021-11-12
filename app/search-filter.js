07145b5466d3958229017b5c7cb3225c2db75f7a
Justine <ujusine@yahoo.com>
2021-11-12 18:45:19 +0800

    toggle active button

--- app/search-filter.js
+++ app/search-filter.js
@@ -1,10 +1,26 @@
 window.addEventListener("load", ()=>{
     const dropdownBtn = document.querySelector(".more-drop-btn");
 	const dropOption = document.querySelector(".drop-option");
+	const relevanceBtn = document.querySelector('.relevance-btn');
+	const newestBtn = document.querySelector('.newest-btn');
 
     	//Hide show drop option in filter
 	dropdownBtn.addEventListener("click", ()=>{
-		console.log("click")
         dropOption.classList.toggle("d-flex");
+	
+	})
+
+	relevanceBtn.addEventListener("click", ()=>{
+
+		relevanceBtn.classList.add('filter-select');
+		newestBtn.classList.remove('filter-select');
+
+	})
+
+	newestBtn.addEventListener('click', ()=>{
+
+		newestBtn.classList.add('filter-select');
+		relevanceBtn.classList.remove('filter-select');
+
 	})
 })
